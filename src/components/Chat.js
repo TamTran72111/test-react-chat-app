22e2ca9dd81f36e1f1f22182cdef81b9fec8b912
TamTran72111 <tam.tran72111@gmail.com>
2020-05-24 15:28:10 -0400

    Extract message history to its own component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Chat.css";
 import People from "./People";
-
+import History from "./History";
 export default function Chat() {
   return (
     <div className="container">
@@ -10,69 +10,7 @@ export default function Chat() {
         <div className="inbox_msg">
           <People />
           <div className="mesgs">
-            <div className="msg_history">
-              <div className="incoming_msg">
-                <div className="incoming_msg_img">
-                  {" "}
-                  <img
-                    src="https://ptetutorials.com/images/user-profile.png"
-                    alt="sunil"
-                  />{" "}
-                </div>
-                <div className="received_msg">
-                  <div className="received_withd_msg">
-                    <p>Test which is a new approach to have all solutions</p>
-                    <span className="time_date"> 11:01 AM | June 9</span>
-                  </div>
-                </div>
-              </div>
-              <div className="outgoing_msg">
-                <div className="sent_msg">
-                  <p>Test which is a new approach to have all solutions</p>
-                  <span className="time_date"> 11:01 AM | June 9</span>{" "}
-                </div>
-              </div>
-              <div className="incoming_msg">
-                <div className="incoming_msg_img">
-                  {" "}
-                  <img
-                    src="https://ptetutorials.com/images/user-profile.png"
-                    alt="sunil"
-                  />{" "}
-                </div>
-                <div className="received_msg">
-                  <div className="received_withd_msg">
-                    <p>Test, which is a new approach to have</p>
-                    <span className="time_date"> 11:01 AM | Yesterday</span>
-                  </div>
-                </div>
-              </div>
-              <div className="outgoing_msg">
-                <div className="sent_msg">
-                  <p>Apollo University, Delhi, India Test</p>
-                  <span className="time_date"> 11:01 AM | Today</span>{" "}
-                </div>
-              </div>
-              <div className="incoming_msg">
-                <div className="incoming_msg_img">
-                  {" "}
-                  <img
-                    src="https://ptetutorials.com/images/user-profile.png"
-                    alt="sunil"
-                  />{" "}
-                </div>
-                <div className="received_msg">
-                  <div className="received_withd_msg">
-                    <p>
-                      We work directly with our designers and suppliers, and
-                      sell direct to you, which means quality, exclusive
-                      products, at a price anyone can afford.
-                    </p>
-                    <span className="time_date"> 11:01 AM | Today</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <History />
             <div className="type_msg">
               <div className="input_msg_write">
                 <input
