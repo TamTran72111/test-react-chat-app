9e59b5faf83ee70c030f3484bfe7f13e3943d610
TamTran72111 <tam.tran72111@gmail.com>
2020-05-24 16:39:19 -0400

    Add Websocket to handle send and receive message from chat server

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,30 +1,46 @@
 import React from "react";
+
 import "./Chat.css";
 import People from "./People";
 import History from "./History";
 import MessageBox from "./MessageBox";
 
-export default function Chat() {
-  return (
-    <div className="container">
-      <h3 className=" text-center">Messaging</h3>
-      <div className="messaging">
-        <div className="inbox_msg">
-          <People />
-          <div className="mesgs">
-            <History />
-            <MessageBox />
+export default class Chat extends React.Component {
+  state = { messages: [] };
+  componentDidMount() {
+    const END_POINT = "localhost:8000/";
+    this.socket = new WebSocket("ws://" + END_POINT + "ws/chat/lobby/");
+    this.socket.onmessage = this.receiveMessage;
+    this.socket.onclose = function (e) {
+      console.error("Chat socket closed unexpectedly");
+    };
+  }
+
+  receiveMessage = (e) => {
+    const data = JSON.parse(e.data);
+    const messages = this.state.messages;
+    messages.push(data.message);
+    this.setState({ messages: messages });
+  };
+
+  sendMessage = (message) => {
+    this.socket.send(JSON.stringify({ message }));
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h3 className=" text-center">Messaging</h3>
+        <div className="messaging">
+          <div className="inbox_msg">
+            <People />
+            <div className="mesgs">
+              <History />
+              <MessageBox sendMessage={this.sendMessage} />
+            </div>
           </div>
         </div>
-
-        <p className="text-center top_spac">
-          {" "}
-          Design by{" "}
-          <a target="_blank" href="#">
-            Sunil Rajput
-          </a>
-        </p>
       </div>
-    </div>
-  );
+    );
+  }
 }
--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -1,14 +1,34 @@
 import React from "react";
 
-export default function MessageBox() {
-  return (
-    <div className="type_msg">
-      <div className="input_msg_write">
-        <input type="text" className="write_msg" placeholder="Type a message" />
-        <button className="msg_send_btn" type="button">
-          <i className="fa fa-paper-plane-o" aria-hidden="true"></i>
-        </button>
+export default class MessageBox extends React.Component {
+  state = { message: "" };
+  sendMessage = () => {
+    this.props.sendMessage(this.state.message);
+    this.setState({ message: "" });
+  };
+  handleChange = (e) => {
+    this.setState({ message: e.target.value });
+  };
+  render() {
+    return (
+      <div className="type_msg">
+        <div className="input_msg_write">
+          <input
+            type="text"
+            className="write_msg"
+            placeholder="Type a message"
+            value={this.state.message}
+            onChange={this.handleChange}
+          />
+          <button
+            className="msg_send_btn"
+            type="button"
+            onClick={this.sendMessage}
+          >
+            <i className="fa fa-paper-plane-o" aria-hidden="true"></i>
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
